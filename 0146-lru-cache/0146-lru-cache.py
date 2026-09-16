f32df96dc51bb5844bf74d2078243ba76c878749
Harvey Specter <148318869+Lordwhis@users.noreply.github.com>
2026-09-16 08:44:14 +0530

    Time: 28 ms (73.97%), Space: 29.3 MB (76.66%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,75 +1,48 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
-    class Node:
-        def __init__(self, key=0, value=0):
-            self.key = key
-            self.value = value
-            self.prev = None
-            self.next = None
-
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.cap = capacity
         self.cache = {}
 
-        # Dummy nodes
-        self.head = self.Node()  # Most recently used side
-        self.tail = self.Node()  # Least recently used side
-
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def remove(self, node):
-        """Remove node from linked list."""
-        prev_node = node.prev
-        next_node = node.next
-
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def add_to_front(self, node):
-        """Add node right after head."""
-        node.next = self.head.next
-        node.prev = self.head
-
-        self.head.next.prev = node
-        self.head.next = node
+        self.oldest = Node(0, 0)
+        self.latest = Node(0, 0)
+        self.oldest.next = self.latest
+        self.latest.prev = self.oldest
+        
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
-        node = self.cache[key]
-
-        # Recently accessed => move to front
-        self.remove(node)
-        self.add_to_front(node)
-
-        return node.value
+    def remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next = next
+        next.prev = prev
+    
+    def insert(self, node):
+        prev, next = self.latest.prev, self.latest
+        prev.next = next.prev = node
+        node.next = next
+        node.prev = prev
 
     def put(self, key: int, value: int) -> None:
-
-        # Key already exists
         if key in self.cache:
-            node = self.cache[key]
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-            node.value = value
-
-            # Updated key becomes most recently used
-            self.remove(node)
-            self.add_to_front(node)
-
-            return
-
-        # Create new node
-        node = self.Node(key, value)
-
-        self.cache[key] = node
-        self.add_to_front(node)
-
-        # Capacity exceeded
-        if len(self.cache) > self.capacity:
-            # Least recently used node is before tail
-            lru = self.tail.prev
-
+        if len(self.cache) > self.cap:
+            lru = self.oldest.next
             self.remove(lru)
             del self.cache[lru.key]
+        
